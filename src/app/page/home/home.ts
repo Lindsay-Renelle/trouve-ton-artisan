7b4277db9d16a803a1d7f26b461aa3a1ada856ac
Dubois Celine <celine.dubois17@free.fr>
2026-04-27 17:56:01 +0200

    Modification dans Home.html + Home.ts

--- src/app/page/home/home.ts
+++ src/app/page/home/home.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule,CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
 
+    etapes = [
+      { id: 1, texte: "Choisir la catégorie d’artisanat dans le menu" },
+      { id: 2, texte: "Choisir un artisan" },
+      { id: 3, texte: "Le contacter via le formulaire de contact" },
+      { id: 4, texte: "Une réponse sera apportée sous 48h" },
+    ];
+  
+    termeRecherche = "";
+  
+    rechercher() {
+      console.log("Recherche :", this.termeRecherche);
+    }
 }

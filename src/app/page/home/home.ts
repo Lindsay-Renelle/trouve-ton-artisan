5e2f8ca4822ace9d94ce80f37065c9da56f1c726
Dubois Celine <celine.dubois17@free.fr>
2026-04-28 17:57:07 +0200

    Modification

--- src/app/page/home/home.ts
+++ src/app/page/home/home.ts
@@ -5,22 +5,25 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrl: './home.scss'
 })
 export class Home {
 
-    etapes = [
-      { id: 1, texte: "Choisir la catégorie d’artisanat dans le menu" },
-      { id: 2, texte: "Choisir un artisan" },
-      { id: 3, texte: "Le contacter via le formulaire de contact" },
-      { id: 4, texte: "Une réponse sera apportée sous 48h" },
-    ];
-  
-    termeRecherche = "";
-  
-    rechercher() {
-      console.log("Recherche :", this.termeRecherche);
-    }
+  // Étapes affichées dans la page
+  etapes = [
+    { id: 1, texte: "Choisir la catégorie d’artisanat dans le menu" },
+    { id: 2, texte: "Choisir un artisan" },
+    { id: 3, texte: "Le contacter via le formulaire de contact" },
+    { id: 4, texte: "Une réponse sera apportée sous 48h" },
+  ];
+
+  // Texte saisi dans la barre de recherche
+  termeRecherche = "";
+
+  // Action du bouton "Rechercher"
+  rechercher() {
+    console.log("Recherche :", this.termeRecherche);
+  }
 }
